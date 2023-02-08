f930193e670bae205551f0b50ea24b0466005bb6
henritresor <shimwamanahenritresor@gmail.com>
2023-02-08 07:55:55 +0200

    added environment variables

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const { Server } = require("socket.io");
+require('dotenv').config()
 const app = express();
 const http = require("http");
 const { hostname } = require("os");
 const server = http.createServer(app);
-const PORT = 3030;
+const PORT = process.env.PORT;
 const io = new Server(server)
 app.use(express.static('public'))
-const ip = `192.168.1.213`
+const ip = process.env.IPV4_ADDR;
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + '/index.html');
