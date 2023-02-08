56553f8b77036df2dd4050b7ee558e58361a2ff6
henritresor <shimwamanahenritresor@gmail.com>
2023-02-08 07:57:15 +0200

    added environment variables

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
 const express = require("express");
 const { Server } = require("socket.io");
-require('dotenv').config()
+require("dotenv").config();
 const app = express();
 const http = require("http");
 const { hostname } = require("os");
 const server = http.createServer(app);
 const PORT = process.env.PORT;
-const io = new Server(server)
-app.use(express.static('public'))
+const io = new Server(server);
+app.use(express.static("public"));
 const ip = process.env.IPV4_ADDR;
 
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+  res.sendFile(__dirname + "/index.html");
 });
 
-io.on('connection', (socket) => {
-  console.log('user connected');
-  socket.on('msg', (msg,username) => {
-    io.emit('msg',msg,username)
-  })
-})
+io.on("connection", (socket) => {
+  console.log("user connected");
+  socket.on("msg", (msg, username) => {
+    io.emit("msg", msg, username);
+  });
+});
 
 server.listen(PORT, ip, () => {
   console.log(`server listening on ${ip}:${PORT}`);
